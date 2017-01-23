6d926552d95ca219a1511a8bd479192853cb0941
Krzysiek Witkowicz <krzysiekwitkowicz@gmail.com>
2017-01-23 20:26:52 +0100

    Refactoring according to PEP8w

--- temp_logger.py
+++ temp_logger.py
@@ -1,16 +1,17 @@
 import threading
 import time
 import datetime
-import random
 import sqlite3
 import math
 import Adafruit_DHT
+
 
 class TempLogger(threading.Thread):
     isRunning = True
 
     def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs=None, verbose=None, interval = 5, in_memory_db = True):
+                 args=(), kwargs=None, verbose=None, interval=5,
+                 in_memory_db=True):
         threading.Thread.__init__(self, group=group, target=target, name=name,
                                   verbose=verbose)
         self.conn = None
@@ -20,13 +21,9 @@ class TempLogger(threading.Thread):
         self._setup()
 
     def _setup(self):
-        #self.sensor
         self.start_db()
 
     def run(self):
-        """
-ProgrammingError: SQLite objects created in a thread can only be used in that same thread.The object was created in thread id 10052 and this is thread id 4532
-"""
         while self.isRunning:
             self._make_measurement()
             time.sleep(self.interval)
@@ -36,85 +33,85 @@ ProgrammingError: SQLite objects created in a thread can only be used in that sa
         self.join()
         self.stop_db()
 
-    def calc_dew_point(self,t,h):
-        return math.pow((h/100.0),(1.0/8)) * (112 + (0.9 * t)) + (0.1 * t) - 112
+    def _calc_dew_point(self, t, h):
+        return math.pow((h/100.0), (1.0/8)) * (112 + (0.9 * t)) + (0.1 * t) - 112
 
-    def _make_measurement(self,sensor=Adafruit_DHT.DHT11,pin=4):
+    def _make_measurement(self, sensor=Adafruit_DHT.DHT11, pin=4):
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        humidity,temp =  Adafruit_DHT.read_retry(sensor,pin)
-        dew_point = float("{0:.2f}".format(self.calc_dew_point(temp,humidity)))
-        self.last_measurement = [humidity,temp,dew_point]
-        print(self.last_measurement)
+        humidity, temp = Adafruit_DHT.read_retry(sensor, pin)
+        dew_point = float("{0:.2f}".format(self._calc_dew_point(temp,
+                                                                humidity)))
+        self.last_measurement = [humidity, temp, dew_point]
         cur = self.conn.cursor()
-        cur.execute("Insert into sensor_data(created_at, humidity, temperature, dew_point) values(?,?,?,?)",(now,humidity,temp,dew_point))
-        print "taken at {0}\nhumidity: {1}\ntemperature: {2}\ndew_point: {3}".format(now,humidity,temp,dew_point)
+        cur.execute("""
+                    Insert into sensor_data(created_at, humidity, temperature,
+                    dew_point) values(?,?,?,?)""",
+                    (now, humidity, temp, dew_point))
 
     def get_last_measurement(self):
         if len(self.last_measurement) == 4:
-            self.last_measurement[0]=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            self.last_measurement[0] = datetime.datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S')
         else:
-            self.last_measurement.insert(0,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        print "return last {}".format(self.last_measurement,)
+            self.last_measurement.insert(
+                0, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         return self.last_measurement
-    
-    def get_last_n_rows(self, limit = 10):
+
+    def get_last_n_rows(self, limit=10):
         cur = self.conn.cursor()
-        cur.execute("""select created_at, humidity, temperature from sensor_data order by created_at DESC limit ? """,(limit,))
+        cur.execute("""
+                    select created_at, humidity, temperature from sensor_data
+                    order by created_at DESC limit ? """, (limit, ))
         result = []
         while True:
             row = cur.fetchone()
-            if row == None:
+            if row is None:
                 break
-
-            print row
-            print type(row)
-
             result.append(row)
         return result
 
-    def get_archival_data(self,date_from, date_to):
+    def get_archival_data(self, date_from, date_to):
         cur = self.conn.cursor()
         if date_to is None:
-            print "to is none"
-            cur.execute("select created_at, humidity, temperature, dew_point from sensor_data WHERE created_at > ? order by created_at ASC",(date_from,)) 
+            cur.execute("""select created_at, humidity, temperature,
+                        dew_point from sensor_data WHERE created_at > ?
+                        order by created_at ASC""", (date_from, ))
         else:
-            print "to is not none"
-            cur.execute("select created_at, humidity, temperature, dew_point from sensor_data WHERE created_at > ? AND created_at < ? order by created_at ASC",
-                    (date_from,date_to)) 
+            cur.execute("""select created_at, humidity, temperature, dew_point
+                        from sensor_data WHERE created_at > ? AND
+                        created_at < ? order by created_at ASC""",
+                        (date_from, date_to))
         result = []
         while True:
             row = cur.fetchone()
-            if row == None:
+            if row is None:
                 break
             result.append(row)
         return result
-            
+
     def start_db(self):
+        """
+ProgrammingError: SQLite objects created in a thread can only be used in that
+same thread.The object was created in thread id 10052 and this is
+thread id 4532
+"""
         if self.in_memory_db:
-            self.conn = sqlite3.connect(":memory:",check_same_thread=False)
+            self.conn = sqlite3.connect(":memory:", check_same_thread=False)
             cur = self.conn.cursor()
-            cur.execute("CREATE TABLE sensor_data(id Integer Primary Key, created_at timestamp, humidity real, temperature real, dew_point real)")
+            cur.execute("""CREATE TABLE sensor_data(id Integer Primary Key,
+                        created_at timestamp, humidity real, temperature real,
+                        dew_point real)""")
             cur.close()
             self.conn.commit()
         else:
-            self.conn = sqlite3.connect("logger.db",check_same_thread=False)
-       
-    def stop_db(self):
-        """
-        cur = self.conn.cursor()
-        cur.execute("Select * from temp")
-        while True:
-            row = cur.fetchone()
-            if row == None:
-                break
+            self.conn = sqlite3.connect("logger.db", check_same_thread=False)
 
-            print row
-        """
-        self.get_last_n_rows()
+    def stop_db(self):
         self.conn.close()
 
-if __name__=='__main__':
-    m = TempLogger() 
+if __name__ == '__main__':
+    m = TempLogger()
     m.start()
     time.sleep(5)
+    print m.get_last_measurement()
     m.stop()
--- temp_server.py
+++ temp_server.py
@@ -1,8 +1,6 @@
-
 import cherrypy
-import threading
-import time
 from temp_logger import TempLogger
+
 
 class TempServer(object):
 
@@ -11,11 +9,11 @@ class TempServer(object):
 
     @cherrypy.expose
     def index(self):
-        return open ('index.html')
+        return open('index.html')
 
     @cherrypy.expose
     def historic(self):
-        return open ('historic.html')
+        return open('historic.html')
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
@@ -24,30 +22,30 @@ class TempServer(object):
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
-    def get_historical_data(self,start_date,stop_date):
-        if stop_date =='':
-            stop_date=None 
-        return self.logger.get_archival_data(start_date,stop_date)
+    def get_historical_data(self, start_date, stop_date):
+        if stop_date == '':
+            stop_date = None
+        return self.logger.get_archival_data(start_date, stop_date)
 
     def run_background_task(self):
         self.logger.start()
-    
+
     def stop_background_task(self):
         self.logger.stop()
 
 if __name__ == '__main__':
     global_config = {
-            'global':{
+            'global': {
                 'server.socket_host': '0.0.0.0',
                 'server.socket_port': 8080,
                 'log.screen': True,
                 'engine.autoreload.on': False,
                 'engine.SIGHUP': None,
                 'engine.SIGTERM': None
-    } 
+            }
             }
     server = TempServer()
     print 'Server is running.'
-    cherrypy.engine.subscribe('start',server.run_background_task)
-    cherrypy.engine.subscribe('stop',server.stop_background_task)
+    cherrypy.engine.subscribe('start', server.run_background_task)
+    cherrypy.engine.subscribe('stop', server.stop_background_task)
     cherrypy.quickstart(server, '/', global_config)
