12465c7c3f7f484c79793bd82f66692f2a44671e
Krzysiek Witkowicz <krzysiekwitkowicz@gmail.com>
2017-05-17 22:05:59 +0200

    Add config page

--- temp_server.py
+++ temp_server.py
@@ -1,6 +1,33 @@
 import cherrypy
 import sys
+import json
 from temp_logger import TempLogger
+
+
+class Config(object):
+
+    @cherrypy.expose
+    def index(self):
+        return open('config.html')
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_config(self):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+        return data
+        #return {'interval':10}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def save_config(self):
+        print 'in save config'
+        input_json = cherrypy.request.json
+        print input_json
+        with open('config.json', 'w') as outfile:
+                json.dump(input_json, outfile)
+        return {"operation": "request", "result": "success"}
 
 
 class TempServer(object):
@@ -31,6 +58,7 @@ class TempServer(object):
         return self.logger.get_archival_data(start_date, stop_date)
 
     def run_background_task(self):
+        print("run background task")
         self.logger.start()
 
     def stop_background_task(self):
@@ -41,7 +69,7 @@ if __name__ == '__main__':
         in_memory = False
     else:
         in_memory = True
-    global_config = {
+        global_config = {
             'global': {
                 'server.socket_host': '0.0.0.0',
                 'server.socket_port': 8080,
@@ -50,9 +78,25 @@ if __name__ == '__main__':
                 'engine.SIGHUP': None,
                 'engine.SIGTERM': None
             }
+        }
+        cfg_config = {
+            '/': {
+                # the api uses restful method
+                # dispatching
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+
+                # all api calls require that the
+                # client passes HTTP basic authentication
+
             }
+
+        }
     server = TempServer(in_memory_db=in_memory)
     print 'Server is running.'
     cherrypy.engine.subscribe('start', server.run_background_task)
     cherrypy.engine.subscribe('stop', server.stop_background_task)
+    cherrypy.tree.mount(server, '/', config=global_config)
+    cherrypy.tree.mount(Config(), '/config', config=global_config)
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', })
+    cherrypy.config.update({'server.socket_port': 8080, })
     cherrypy.quickstart(server, '/', global_config)
